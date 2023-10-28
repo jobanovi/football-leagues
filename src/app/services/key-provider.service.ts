0c4107945d8abd3b79d7508f5b8e4eb7e54ff976
Josip Banović <josipbanovic@onetrail.com>
2023-10-28 11:59:22 +0200

    added new key and changed getting valid key

--- src/app/services/key-provider.service.ts
+++ src/app/services/key-provider.service.ts
@@ -5,17 +5,22 @@ import {Injectable} from '@angular/core';
 })
 export class KeyProviderService {
 
-  private readonly keys: KeyInfo[] = [{key: "2b1404d1ca10c9f47715c89318432cad", isValid: true},
-    {key: "0700862f2c8329dd671a20b8c96e58bb", isValid: true},
-    {key: "a0efa8d3ccc54bd4d678b69fd4e67480", isValid: true}];
+  // invalid keys: 2b1404d1ca10c9f47715c89318432cad 0700862f2c8329dd671a20b8c96e58bb a0efa8d3ccc54bd4d678b69fd4e67480
+  //
+  private readonly keys: KeyInfo[] = [{key: "516499c256cbdf600f3adacb3105be04", isValid: true}];
 
   /**
-   * Gets first valid key or, if all are invalid, first one
+   * Gets first valid key or, if all are invalid, first one because most probably rate limit has expired for that one
    */
   getValidKey(): KeyInfo {
     console.log("getting another valid key");
     const validKey: KeyInfo | undefined = this.keys.find(k => k.isValid);
-    return validKey ? validKey : this.keys.at(0) as KeyInfo;
+    if (!validKey) {
+      // mark all keys as valid to avoid marking key as invalid even though rate limit expired (especially for minute)
+      this.keys.forEach(k => k.isValid = true);
+      return this.keys.at(0) as KeyInfo;
+    }
+    return validKey;
   }
 
   updateKey(keyInfo: KeyInfo) {
